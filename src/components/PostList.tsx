955688fdb5feb758c83b146d9f543b2c1fe6f45d
Erick <oneerick3@gmail.com>
2026-07-20 13:17:46 +0300

    fix: set revalidate to 0 for immediate data fetching in PostsList component

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,7 +4,7 @@ import { client } from "@/sanity/client";
 import { ArrowRight } from "lucide-react";
 import ScrollableRow from "./ScrollableRow";
 
-export const revalidate = 30;
+export const revalidate = 0;
 
 const CATEGORY_WITH_POSTS_QUERY = `
   *[_type == "category"]{
